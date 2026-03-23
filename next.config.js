41cb9b206fa60a1c5541ef8fbe6ee4c650df2cd4
cauagtr <92761013+cauagtr@users.noreply.github.com>
2026-03-23 13:53:04 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,12 +3,6 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['xlsx'],
   },
-  api: {
-    bodyParser: {
-      sizeLimit: '50mb',
-    },
-    responseLimit: '50mb',
-  },
 }
 
 module.exports = nextConfig
